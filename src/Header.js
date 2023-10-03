84249a51e34358a8c3fefb105d67f221affe13df
Dylan Bailey <wearesleepless@gmail.com>
2023-10-03 10:56:31 -0400

    Initial Commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return(
+        <header>
+            <p>
+                <i className="fa-solid fa-earth-americas"></i>
+                my travel journal.
+            </p>
+        </header>
+    );
+}
+
+export default Header;
